9dcfc8ff70c79ca8ccda141a8772b0751ea4b733
1002860741@qq.com <1002860741@qq.com>
2018-08-15 11:25:44 +0800

    fix code test

--- common/config/test.php
+++ common/config/test.php
@@ -4,6 +4,7 @@ return [
     'basePath' => dirname(__DIR__),
     'components' => [
         'user' => [
+		//添加测试文件
             'class' => 'yii\web\User',
             'identityClass' => 'common\models\User',
         ],
