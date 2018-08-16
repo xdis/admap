aa5b8f16f111bee9a526d4f4e96e18234b1a6c9c
bailefei <1002860741@qq.com>
2018-08-16 10:00:18 +0800

    提交dev-test代码

--- common/config/test.php
+++ common/config/test.php
@@ -4,7 +4,7 @@ return [
     'basePath' => dirname(__DIR__),
     'components' => [
         'user' => [
-    //添加测试文件ee rrrrr   测试 3444     55555 778   这是是功能开发代码   dev开发代码  dev-test分支代码
+    //添加测试文件ee rrrrr   测试 3444     55555 778   这是是功能开发代码   dev开发代码  dev-test分支代码   本地代码提交git代码到dev环境
             'class' => 'yii\web\User',
             'identityClass' => 'common\models\User',
         ],
