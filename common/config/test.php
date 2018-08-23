0f492333fbba8a8924769f9cfca7de683500aea3
bailefei <1002860741@qq.com>
2018-08-23 16:39:59 +0800

    我要提交代码了

--- common/config/test.php
+++ common/config/test.php
@@ -4,7 +4,7 @@ return [
     'basePath' => dirname(__DIR__),
     'components' => [
         'user' => [
-    //开始测试的
+    //开始测试的  从dev-test推代码到develop
 	//提交代码12345
             'class' => 'yii\web\User',
             'identityClass' => 'common\models\User',
